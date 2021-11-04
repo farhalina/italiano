4db27786b6110b84ce82eeaf04f4d3ec4a0b971d
Farha Lina <farhalin@gmail.com>
2021-11-04 15:35:09 -0400

    mapping through words

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Translate from './Translate';
 
 function App() {
   return (
     <div className="App">
       <h1>Italiano</h1>
+      <Translate/>
     </div>
   );
 }
---
+++ src/Translate.js
@@ -0,0 +1,11 @@
+import ListOfWords from "./ListOfWords";
+
+function Translate({allWords}){
+    return (
+        <div>
+           {allWords}
+        </div>
+    )
+}
+
+export default Translate;
